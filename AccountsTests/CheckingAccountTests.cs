24fe78688eec261115a6279ad6791051523d6b21
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-02-21 20:50:09 +0100

    Adding Tests to Withdraw method of CheckingAccount class

--- AccountsTests/CheckingAccountTests.cs
+++ AccountsTests/CheckingAccountTests.cs
@@ -1,11 +1,38 @@
-namespace AccountsTests
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Accounts;
+
+namespace Accounts.Tests
 {
-    [TestClass]
+    [TestClass()]
     public class CheckingAccountTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestMethod()]
+        [Timeout(2000)]
+        public void Withdraw_ValidAmount_ChangesBalance()
         {
+            // Arrange
+            double currentBalance = 10.0;
+            double amount = 1.0;
+            double expected = 9.0;
+            var account = new CheckingAccount(currentBalance);
+
+            // Act
+            account.Withdraw(amount);
+
+            //Assert
+            Assert.AreEqual(expected, account.Balance);
+        }
+
+        [TestMethod]
+        [Timeout(TestTimeout.Infinite)]
+        public void Withdraw_AmountMoreThanBalance_ThrowsException()
+        {
+            // arrange
+            double currentBalance = 10.0;
+            var account = new CheckingAccount(currentBalance);
+
+            // act and assert
+            Assert.ThrowsException<ArgumentException>(() => account.Withdraw(20.0));
         }
     }
 }
