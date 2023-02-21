8de0d03552d7017cdb1d6349d1b92c5246eeb3a1
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-02-21 15:17:30 +0100

    Write The test first

--- MathTests/UnitTest1.cs
+++ MathTests/UnitTest1.cs
@@ -4,8 +4,20 @@ namespace MathTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BasicRooterTest()
         {
+            // Create an instance to test:
+            Rooter rooter = new Rooter();
+
+            // Define a test input and output value:
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+
+            // Run the method under test:
+            double actualResult = rooter.SquareRoot(input);
+
+            // Verify the result:
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
         }
     }
 }
