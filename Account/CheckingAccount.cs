e74ba76925552104c721811f9de7b27b4bcb0daa
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-02-21 18:51:00 +0100

    Extand IAccount interface by CheckingAccount class and implement Withdraw method

--- Account/CheckingAccount.cs
+++ Account/CheckingAccount.cs
@@ -1,7 +1,23 @@
-﻿namespace Account
+﻿namespace Accounts
 {
-    public class CheckingAccount
+    public class CheckingAccount : IAccount
     {
+        private double m_balance;
 
+        public CheckingAccount(double balance)
+        {
+            m_balance = balance;
+        }
+        public void Withdraw(double amount)
+        {
+            if (m_balance >= amount)
+            {
+                m_balance -= amount;
+            }
+            else
+            {
+                throw new ArgumentException("Withdraw exceeds balance!.", nameof(amount));
+            }
+        }
     }
 }
---
+++ AccountsTests/CheckingAccountTests.cs
@@ -0,0 +1,11 @@
+namespace AccountsTests
+{
+    [TestClass]
+    public class CheckingAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
