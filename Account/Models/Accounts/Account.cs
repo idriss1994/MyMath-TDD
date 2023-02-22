dde6e052ffdf3cef232027a0d691a22dd2d1011d
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-02-22 22:18:50 +0100

    Adding Account model

---
+++ Account/Models/Accounts/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts.Models.Accounts
+{
+    public sealed class Account
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+    }
+}
