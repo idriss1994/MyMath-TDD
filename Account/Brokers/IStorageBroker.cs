5849534374e95ff22cf3d07888883805c9b195ac
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-02-22 22:18:36 +0100

    Adding storage broker

---
+++ Account/Brokers/IStorageBroker.cs
@@ -0,0 +1,14 @@
+﻿using Accounts.Models.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts.Brokers
+{
+    public interface IStorageBroker
+    {
+        Account InsertAccount(Account account);
+    }
+}
