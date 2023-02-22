418b4802d3ae29dfa46d87bafdbd9ac6bb9a4abb
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-02-22 22:19:08 +0100

    Adding Account Services

---
+++ Account/Services/Accounts/IAccountService.cs
@@ -0,0 +1,30 @@
+﻿using Accounts.Brokers;
+using Accounts.Models.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts.Services.Accounts
+{
+    public interface IAccountService
+    {
+        Account AddAccount(Account account);
+    }
+
+    public class AccountService : IAccountService
+    {
+        private readonly IStorageBroker _storageBroker;
+        public AccountService(IStorageBroker storageBroker)
+        {
+            _storageBroker = storageBroker;
+        }
+
+        //Write Test first (TDD).
+        public Account AddAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
