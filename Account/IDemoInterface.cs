6d0f79bb375ba864e670fb7d2ac26bdda32912f7
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-02-23 21:31:15 +0100

    ShouldAddAccount => Fail

---
+++ Account/IDemoInterface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    public interface IDemoInterface
+    {
+     
+        static int age;
+        void Method()
+        {
+            Console.WriteLine("This a method with body brom interface");
+        }
+    }
+
+    public abstract class Person
+    {
+        static int age;
+        public Person()
+        {
+            age = 1;
+        }
+        public void PersonMethod()
+        {
+            Console.WriteLine("Hi, I am a person");
+        }
+    }
+    public class Student : Person, IDemoInterface
+    {
+        Person person;
+        public Student() : base()
+        {
+            
+            person = new Student();
+        }
+
+        public void Method()
+        {
+            // Some work
+        }
+    }
+}
